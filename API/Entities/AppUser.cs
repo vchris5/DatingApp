7a91eb5056586b619bb79de82a95722c327a7b7c
Christian S <79862916+vchris5@users.noreply.github.com>
2024-03-25 12:37:46 -0400

    Adding auth middleware and extension methods | 3-25-010010

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,11 @@
-﻿namespace API.Entities;
-
-public class AppUser
+﻿namespace API.Entities
 {
-    public int Id { get; set; }
-    public string UserName { get; set; }
+
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,12 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -19,6 +15,7 @@ app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("ht
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
